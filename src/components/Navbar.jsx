e4d072598a9c87fb16fae0a9aa056eaddce9b90f
mariosknl <mariosknl@outlook.com>
2023-01-04 13:36:35 +0200

    web3 context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+import { useStateContext } from "../context";
 import { CustomButton } from "./";
 import { logo, menu, search, thirdweb } from "../assets";
 import { navlinks } from "../constants";
@@ -9,8 +10,7 @@ const Navbar = () => {
 	const navigate = useNavigate();
 	const [isActive, setIsActive] = useState("dashboard");
 	const [toggleDrawer, setToggleDrawer] = useState(false);
-
-	const address = "0xabc";
+	const { address, connect } = useStateContext();
 
 	return (
 		<div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
@@ -37,7 +37,7 @@ const Navbar = () => {
 					styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
 					handleClick={() => {
 						if (address) navigate("create-campaign");
-						else "connect()";
+						else connect();
 					}}
 				/>
 
@@ -112,7 +112,7 @@ const Navbar = () => {
 							styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
 							handleClick={() => {
 								if (address) navigate("create-campaign");
-								else "connect()";
+								else connect();
 							}}
 						/>
 					</div>
